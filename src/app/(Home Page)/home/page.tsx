d77431f1b3f11246c849682f4a8c8cfd7bd7ce65
Seif Eldin Sameh <seiffsameh00@gmail.com>
2024-03-29 15:26:15 +0200

    Update Under Construction

--- src/app/(Home Page)/home/page.tsx
+++ src/app/(Home Page)/home/page.tsx
@@ -1,7 +1,23 @@
+import localFont from "next/font/local";
+
+// Tanker font
+const tanker = localFont({
+  src: [
+    {
+      path: "../../../public/fonts/Tanker/Tanker-Regular.woff2",
+      weight: "400",
+      style: "normal",
+    },
+  ],
+  display: "swap",
+});
+
 export default function page() {
   return (
     <main className="home-page min-h-screen flex justify-center items-center">
-      <h1>Under Construction</h1>
+      <h1 className={`${tanker.className} min-[290px]:text-4xl md:text-8xl`}>
+        Under Construction
+      </h1>
     </main>
   );
 }
