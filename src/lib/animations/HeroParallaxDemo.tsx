e561c98ecdcd5e5a2701ae5364917b8e8adfa3cd
Seif Eldin Sameh <seiffsameh00@gmail.com>
2024-06-19 16:05:37 +0300

    Hero images updated

--- src/lib/animations/HeroParallaxDemo.tsx
+++ src/lib/animations/HeroParallaxDemo.tsx
@@ -8,77 +8,62 @@ export function HeroParallaxDemo() {
 export const products = [
   {
     title: "Enggam",
-    // link: "https://enggam.kinde.com/auth/cx/_:nav&m:login&psid:c41823949fdc4ce7b1aafc45c10cc5e2",
     thumbnail: "/assets/hero-1.webp",
   },
   {
     title: "Enggam",
-    // link: "https://enggam.kinde.com/auth/cx/_:nav&m:login&psid:c41823949fdc4ce7b1aafc45c10cc5e2",
     thumbnail: "/assets/hero-2.webp",
   },
   {
     title: "Enggam",
-    // link: "https://enggam.kinde.com/auth/cx/_:nav&m:login&psid:c41823949fdc4ce7b1aafc45c10cc5e2",
     thumbnail: "/assets/hero-3.webp",
   },
   {
     title: "Enggam",
-    // link: "https://enggam.kinde.com/auth/cx/_:nav&m:login&psid:c41823949fdc4ce7b1aafc45c10cc5e2",
-    thumbnail: "/assets/hero-2.webp",
+    thumbnail: "/assets/hero-4.webp",
   },
   {
     title: "Enggam",
-    // link: "https://enggam.kinde.com/auth/cx/_:nav&m:login&psid:c41823949fdc4ce7b1aafc45c10cc5e2",
-    thumbnail: "/assets/hero-3.webp",
+    thumbnail: "/assets/hero-5.webp",
   },
   {
     title: "Enggam",
-    // link: "https://enggam.kinde.com/auth/cx/_:nav&m:login&psid:c41823949fdc4ce7b1aafc45c10cc5e2",
     thumbnail: "/assets/hero-6.webp",
   },
   {
     title: "Enggam",
-    // link: "https://enggam.kinde.com/auth/cx/_:nav&m:login&psid:c41823949fdc4ce7b1aafc45c10cc5e2",
     thumbnail: "/assets/hero-1.webp",
   },
   {
     title: "Enggam",
-    // link: "https://enggam.kinde.com/auth/cx/_:nav&m:login&psid:c41823949fdc4ce7b1aafc45c10cc5e2",
     thumbnail: "/assets/hero-2.webp",
   },
   {
     title: "Enggam",
-    // link: "https://enggam.kinde.com/auth/cx/_:nav&m:login&psid:c41823949fdc4ce7b1aafc45c10cc5e2",
     thumbnail: "/assets/hero-3.webp",
   },
   {
     title: "Enggam",
-    // link: "https://enggam.kinde.com/auth/cx/_:nav&m:login&psid:c41823949fdc4ce7b1aafc45c10cc5e2",
-    thumbnail: "/assets/hero-1.webp",
+    thumbnail: "/assets/hero-4.webp",
   },
   {
     title: "Enggam",
-    // link: "https://enggam.kinde.com/auth/cx/_:nav&m:login&psid:c41823949fdc4ce7b1aafc45c10cc5e2",
-    thumbnail: "/assets/hero-2.webp",
+    thumbnail: "/assets/hero-5.webp",
   },
   {
     title: "Enggam",
-    // link: "https://enggam.kinde.com/auth/cx/_:nav&m:login&psid:c41823949fdc4ce7b1aafc45c10cc5e2",
     thumbnail: "/assets/hero-6.webp",
   },
   {
     title: "Enggam",
-    // link: "https://enggam.kinde.com/auth/cx/_:nav&m:login&psid:c41823949fdc4ce7b1aafc45c10cc5e2",
     thumbnail: "/assets/hero-1.webp",
   },
   {
     title: "Enggam",
-    // link: "https://enggam.kinde.com/auth/cx/_:nav&m:login&psid:c41823949fdc4ce7b1aafc45c10cc5e2",
     thumbnail: "/assets/hero-2.webp",
   },
   {
     title: "Enggam",
-    // link: "https://enggam.kinde.com/auth/cx/_:nav&m:login&psid:c41823949fdc4ce7b1aafc45c10cc5e2",
     thumbnail: "/assets/hero-3.webp",
   },
 ];
