6ab0fa851ab1b6f0d2ca6dd0d363724e7472ad10
Eng/Ali <88795968+mannoo123@users.noreply.github.com>
2024-06-26 17:51:04 +0300

    Update subCategoriesProducts.ts

--- constants/subCategoriesProducts.ts
+++ constants/subCategoriesProducts.ts
@@ -4659,14 +4659,4 @@ export const subCategoryProducts = [
     country: "Egypt",
     related: "psn",
   },
-  {
-    id: generateUniqueId(),
-    image:
-      "https://res.cloudinary.com/dp9iqarvw/image/upload/v1709456772/Enggam/maskable_icon_p0oanz.png",
-    name: "PSN Card",
-    description: "refrfrrfrfrfrfrfrfrfrf ",
-    price: 12.00,
-    country: "sudan",
-    related: "psn",
-  },
 ];
