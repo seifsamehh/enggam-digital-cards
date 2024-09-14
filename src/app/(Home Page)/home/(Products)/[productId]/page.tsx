78626df97b856d4ed063ff80fa7dc58c667d2f57
seifsamehh <seifsameh1432@gmail.com>
2024-09-14 09:26:52 +0300

    Products test

--- src/app/(Home Page)/home/(Products)/[productId]/page.tsx
+++ src/app/(Home Page)/home/(Products)/[productId]/page.tsx
@@ -36,6 +36,8 @@ const ProductDetails = () => {
     const fetchProduct = () => {
       const allProducts = [...sellerProducts, ...saleProducts];
 
+      console.log(allProducts);
+
       const foundProduct = allProducts.find(
         (item) => item.id === params.productId
       );
